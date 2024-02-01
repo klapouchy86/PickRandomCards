6e302e790485d8ae181ada049659a6b42eb738d1
klapouchy86 <m.jelski86@gmail.com>
2024-02-01 19:14:03 +0100

    Dodaj pliki projektów.

---
+++ PickRandomCards/CardPicker.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PickRandomCards
+{
+    internal class CardPicker
+    {
+        public static string[] PickSomeCards(int numberOfCards)
+        {
+            string[] pickedCards = new string[numberOfCards];
+            for (int i = 0; i < numberOfCards; i++) 
+            {
+                pickedCards[i] = RandomValue() + " " + RandomSuit();
+            }
+            return pickedCards;
+        }
+    }
+}
