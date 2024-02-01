e5258bb6996b661b50c23b67cf1d54bca26470f4
klapouchy86 <m.jelski86@gmail.com>
2024-02-01 19:44:22 +0100

    Main metod completed

--- PickRandomCards/Program.cs
+++ PickRandomCards/Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Write("Wpisz liczbę generowanych kart: ");
+            string line = Console.ReadLine();
+
+            if (int.TryParse(line, out int numberOfCards)) 
+            {
+                foreach (string card in CardPicker.PickSomeCards(numberOfCards))
+                {
+                    Console.WriteLine(card);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Wpisz prawidłową liczbę.");
+            }
+
         }
     }
 }
